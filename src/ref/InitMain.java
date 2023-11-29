31ca469113145e9f9a55991409ffdc15badaf879
orion <mrke0916@naver.com>
2023-11-29 22:48:30 +0900

    변수: 기본형과 참조형 그리고 null

---
+++ src/ref/InitMain.java
@@ -0,0 +1,9 @@
+package ref;
+
+public class InitMain {
+    public static void main(String[] args) {
+        InitData data = new InitData();
+        System.out.printf("value 1 = " + data.value1);
+        System.out.printf("value 2 = " + data.value2);
+    }
+}
---
+++ src/ref/NullMain1.java
@@ -0,0 +1,38 @@
+package ref;
+
+public class NullMain1 {
+    public static void main(String[] args) {
+
+        /* null과 참조값 확인
+        Data data = null;
+        System.out.println("1. data = " + data);
+        data = new Data();
+        System.out.println("2. data = " + data);
+        data = null;
+        System.out.println("3. data = " + data);
+         */
+
+        /* nullException 체크
+        Data data = null;
+        data.value = 10;
+        System.out.printf("data = "+ data.value);
+         */
+
+        /*
+        BigData bigData = new BigData();
+        System.out.println("bigData.count = " + bigData.count);
+        System.out.println("bigData.data = " + bigData.data);
+
+        //NullPointerExcetion
+        System.out.println("bigData.data.value=" + bigData.data.value);
+         */
+        
+        BigData bigData = new BigData();
+        bigData.data = new Data();
+        System.out.println("bigData.count = " + bigData.count);
+        System.out.println("bigData.data = " + bigData.data);
+        System.out.println("bigData.data.value = " + bigData.data.value);
+
+
+    }
+}
